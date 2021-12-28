fccc1367306cf05c876b305f3d85dbb9f49dd5d6
Marat Gevorkyan <marat2255a@gmail.com>
2021-12-28 23:32:55 +0300

    add audio uploading

--- web.py
+++ web.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, render_template, request
 import neuro
+
+ALLOWED_EXTENSIONS = {'mp3', 'wav'}
 
 app = Flask(__name__)
 
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    neuro_result = 0
+    neuro_result = None
 
     if request.method == 'POST':
-        data = request.form.get('input1')
-        neuro_result = neuro.neuro(neuro)
+        if 'file' in request.files:
+            data = request.files['file']
+            if data and allowed_file(data.filename):
+                neuro_result = neuro.neuro(neuro)
+            else:
+                neuro_result = 'File is not allowed or no selected file'
 
     return render_template('home.html', neuro_result=neuro_result)
+
+
+app.run(debug=True, host='185.221.153.51', port=5000)
